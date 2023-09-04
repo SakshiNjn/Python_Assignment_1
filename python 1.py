4002408c4f651c760d1799d50b33d0a2161c3081
Sakshi Najan <136469228+SakshiNjn@users.noreply.github.com>
2023-09-04 15:23:55 +0530

    Add files via upload

---
+++ python 1.py
@@ -0,0 +1,12 @@
+a = float(input("Enter the first number (a): "))
+b = float(input("Enter the second number (b): "))
+
+floor_div = a // b
+
+exponential = a ** b
+
+are_equal= a == b
+
+print(f"Floor Division of a // b is : {floor_div}")
+print(f"a power b is: {exponential}")
+print(f"Are a and b equal? {are_equal}")
